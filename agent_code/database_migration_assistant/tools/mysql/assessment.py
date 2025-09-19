0bb42480eb636f03e3cabaa7fe04a58ae552eea3
murlik1 <murlik@google.com>
2025-09-19 05:26:02 +0000

    19-09-2025 adding mysql assessment class structure

--- agent_code/database_migration_assistant/tools/mysql/assessment.py
+++ agent_code/database_migration_assistant/tools/mysql/assessment.py
@@ -2,9 +2,14 @@ from typing import Dict, Any
 from database_migration_assistant.tools.generic import GenericDBAssessment
 
 class MySQLDBAssessment(GenericDBAssessment):
+
     def process_assessment(DBHostName: str,
         DBHostPort:int,
         DBHostUserName: str,
         DBHostUserPass: str
     ) -> Dict[str, Any]:
+        print("Inside Mysql Assessment")
         pass
+
+if __name__ == "__main__":
+    MySQLDBAssessment.process_assessment(DBHostName='a', DBHostPort=1, DBHostUserName='a' ,DBHostUserPass='a')
